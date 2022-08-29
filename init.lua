67a93f3420d01da0bfcc71ccbf1d616ee022f1ec
cora <coradelamouche@gmx.ch>
2022-08-30 00:04:37 +0200

    Add loggers camp

--- init.lua
+++ init.lua
@@ -9,3 +9,4 @@ dofile(mod .. "/desert_oasis.lua") -- Load Desert Oasis
 dofile(mod .. "/birch_forest_ruins.lua") -- Load Birch Forest Ruins
 dofile(mod .. "/brick_pyramid.lua") -- Load Brick Pyramid
 dofile(mod .. "/graveyard.lua") -- Load Graveyards
+dofile(mod .. "/loggers_camp.lua")
